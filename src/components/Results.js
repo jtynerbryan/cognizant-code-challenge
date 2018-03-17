2a0cfc7de89d0fb35577947b75f716a3bbc66567
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-17 13:35:06 -0400

    displaying all sorted data

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,9 +6,32 @@ class Results extends React.Component {
 
   render() {
     console.log(this.state);
+    let sortedBySeaLevel = this.state.sortedBySeaLevel.map((city, index) => {
+      return <div key={index}><h4>{city.city}</h4><h5>sea level: {city.seaLevel}</h5></div>
+    })
     return (
       <div>
-        <h1>Results</h1>
+        <h1 style={{margin: 'auto', textAlign: 'center', width: '50%', marginTop: '20px'}}>Weather Results</h1>
+        <div style={{width: '20%', marginLeft: '20px', display: 'inline-block'}}>
+          <h4>Cites in order by Seal Level (asc):</h4>
+          {sortedBySeaLevel}
+        </div>
+        <div style={{display: 'inline-block', marginLeft: '40px'}}>
+          <h4>Highest Min Temp:</h4>
+          <h4>{this.state.highestMinTemp.name} ({this.state.highestMinTemp.minTemp} degrees)</h4>
+        </div>
+        <div style={{display: 'inline-block', marginLeft: '40px'}}>
+          <h4>Lowest Max Temp:</h4>
+          <h4>{this.state.lowestMaxTemp.name} ({this.state.lowestMaxTemp.maxTemp} degrees)</h4>
+        </div>
+        <div style={{display: 'inline-block', marginLeft: '40px'}}>
+          <h4>Lowest Humidity:</h4>
+          <h4>{this.state.lowestHumidity.name} ({this.state.lowestHumidity.humidity}%)</h4>
+        </div>
+        <div style={{display: 'inline-block', marginLeft: '40px'}}>
+          <h4>Highest Wind Speed:</h4>
+          <h4>{this.state.highestWindSpeed.name} ({this.state.highestWindSpeed.windSpeed}mph)</h4>
+        </div>
       </div>
     )
   }
