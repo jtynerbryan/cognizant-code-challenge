a82adc1668364b312fc5183c55a05f33c8e6f428
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-17 11:47:25 -0400

    successfully averaging data for 5 day forecast

--- src/components/SortData.js
+++ src/components/SortData.js
@@ -12,13 +12,22 @@ class SortData extends React.Component {
       return { city: city.city.name, data: city.list, seaLevel: 0, minTemp: 0, maxTemp: 0, humidity: 0, windSpeed: 0 }
     })
 
-    let sumData = weatherData.forEach(city => {
-      city.data.forEach(local => {
-        city.seaLevel += parseInt(local.main.sea_level, 10)
-        city.minTemp += parseInt(local.main.temp_min, 10)
-        city.maxTemp += parseInt(local.main.temp_max, 10)
-        city.humidity += parseInt(local.main.humidity, 10)
-        city.windSpeed += parseInt(local.wind.speed, 10)
+    // sum data until last iteration, then divide by array length to get average data for 5 day forecast
+    weatherData.forEach(city => {
+      city.data.forEach((forecast, index ) => {
+        if (index+1 === city.data.length) {
+          city.seaLevel /= index+1
+          city.minTemp /= index+1
+          city.maxTemp /= index+1
+          city.humidity /= index+1
+          city.windSpeed /= index+1
+        } else {
+          city.seaLevel +=  parseInt(forecast.main.sea_level, 10)
+          city.minTemp += parseInt(forecast.main.temp_min, 10)
+          city.maxTemp += parseInt(forecast.main.temp_max, 10)
+          city.humidity += parseInt(forecast.main.humidity, 10)
+          city.windSpeed += parseInt(forecast.wind.speed, 10)
+        }
       })
     })
 
